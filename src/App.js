6efd215675d39b4b2f3f3425d38b5c1762a04ac1
tiron <ole_g92@bk.ru>
2022-12-04 21:39:34 +0300

    style and structure changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className='header__nav'>
+          <div className='header__findMe'>
+          <p>Find Me On</p>
+          </div>
+          <div className='header__menu'>
+            <a href='#'>Home</a>
+            <a href='#'>About</a>
+            <a href='#'>Project</a>
+            <a href="#"><button>Contact Me</button></a>
+
+          </div>
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+ 
