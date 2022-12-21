9f5855a7268366d96438fc14cbd7c0b1abe521e1
tiron <ole_g92@bk.ru>
2022-12-21 19:01:58 +0300

    preparing pagination

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const Pagination = () => {
+const getPagesArray = (totalPages) => {
+    const result = [];
+    for (let i = 0; i < totalPages; i++ ) {
+        result.push(i + 1)
+    }
+    return result; 
+}
+
+const Pagination = ({totalPages, page, changePage}) => {
+    console.log(totalPages)
+    let pagesArray = getPagesArray(totalPages);
     return (
-        <div>
-            
-        </div>
+        <div className='projects__page-wrapper'> {pagesArray.map(p => <span onClick={() => changePage(p)} key={p} className={page === p ? 'projects__page projects__page-current' : 'projects__page'}>{p}</span>)} </div>
     );
 };
 
