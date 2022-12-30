7531ad083fb6f28d20265116584e3745734c8252
tiron <ole_g92@bk.ru>
2022-12-30 17:53:05 +0300

    hero style changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,13 @@ const Hero = () => {
     return (
         <section className='hero'>
             <div className='hero__texts'>
+            <div className='hero__wrapper'>
+            <img className='hero__img' src={require('../images/test.jpeg')} alt="hero" />
+            <div className='hero__img-wrapper'></div>
+            <div className='hero__img-plus'></div>
+            <div className='hero__img-slash'></div>
+            </div>
+            <div>
             <h3 className='hero__text'>Hello!</h3>
             <h1 className='hero__title'>I’m <span className='hero__name'>Oleg</span></h1>
             <h3 className='hero__prof'>WEB Developer</h3>
@@ -12,14 +19,11 @@ const Hero = () => {
             <p className='hero__description'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum ligula a euismod luctus. In facilisis nulla quis laoreet ultricies. Sed pharetra urna diam, nec ultrices sapien vestibulum sed. Cras nec libero quis nisl aliquet maximus. Vivamus porttitor tempus tristique. Integer vitae semper magna, nec venenatis dui. Suspendisse sed erat eros. Fusce vel nibh quam. Ut sed sapien massa.
             </p>
+            </div>
+            
             <button className='hero__cv-button'><img src={CV} alt='cv' />Download CV</button>
             </div>
-            <div className='hero__wrapper'>
-            <img className='hero__img' src={require('../images/test.jpeg')} alt="hero" />
-            <div className='hero__img-wrapper'></div>
-            <div className='hero__img-plus'></div>
-            <div className='hero__img-slash'></div>
-            </div>
+           
         </section>
     );
 };
