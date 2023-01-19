eb4040f71087d744cda7a2dee01b27391b8267a4
tiron <ole_g92@bk.ru>
2023-01-19 19:25:36 +0300

    project test page added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
@@ -8,9 +9,11 @@ const NavBar = () => {
           <p>Find Me On</p>
           </div>
           <div className='header__menu'>
-            <a className='header__link' href='/home'>Home</a>
-            <a className='header__link' href='/about'>About</a>
-            <a className='header__link' href='/project'>Project</a>
+            {/* <a className='header__link' href='/home'>Home</a> */}
+            <Link className='header__link' to='/'>Home</Link>
+            {/* <a className='header__link' href='/about'>About</a> */}
+            <Link className='header__link' to='/project'>Project</Link>
+            {/* <a className='header__link' href='/project'>Project</a> */}
             <a className='header__link' href="/contact"><button className='contact'>Contact Me</button></a>
 
           </div>
