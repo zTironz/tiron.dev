015efd815ce939172d0a18cac9627f0dc5bf2137
tiron <ole_g92@bk.ru>
2023-08-15 18:38:12 +0300

    project changes

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -7,14 +7,17 @@ const Project = () => {
     const [text, setText] = useState('')
     const [tag, setTag] = useState('')
 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     function news() {
         fetch('http://localhost:3000/articles')
         .then(res => res.json())
-        .then(data => setStore(data.data))
+        .then(data => setStore(data.articles))
         .catch(err => console.log(err))
     }
     function newNews() {
-        fetch('http://localhost:3000/articles/new', {
+        fetch('http://localhost:3000/articles', {
             method: 'POST',
             body: JSON.stringify({
                 title: title,
@@ -31,6 +34,25 @@ const Project = () => {
         // .catch(err => console.log(err))
     }
 
+    function login() {
+        fetch(`http://localhost:3000/users/login`, {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          credentials: 'include',
+          body: JSON.stringify({
+            "email": email,
+            "password": password
+          }),
+        })
+        .then(res => console.log(res.json())
+        )
+        
+        // .then(data => setStore(data.data))
+        // .catch(err => console.log(err))
+    }
+
     function handleChangeTitle(e) {
         setTitle(e.target.value);
     }
@@ -39,6 +61,14 @@ const Project = () => {
     }
     function handleChangeTag(e) {
         setTag(e.target.value);
+    }
+
+    function handleChangeEmail(e) {
+        setEmail(e.target.value);
+    }
+
+    function handleChangePassword(e) {
+        setPassword(e.target.value);
     }
 
 
@@ -51,12 +81,20 @@ const Project = () => {
             <input type="text" onChange={handleChangeTitle} />
             <input type="text" onChange={handleChangeText} />
             <input type="text" onChange={handleChangeTag} />
+            <br />
+            <br />
+            <input placeholder='email' type="text" onChange={handleChangeEmail} />
+            <input placeholder='password' type="text" onChange={handleChangePassword} />
+            <button style={{color: "red", fontSize: '8px', width: '40px', height: '20px'}} onClick={()=>login()}>Login</button>
+            <br />
+            <br />
             <div style={{color: "red", fontSize: '8px'}}>{
                 store.map(el => 
              <div style={{border: "1px solid black", padding: '2px', margin: '2px'}} key={el._id}>
                 <p>{el.title}</p>
                 <p>{el.text}</p>
                 <p>{el.tag}</p>
+                <p>{el.owner.name}</p>
              </div>
                 )
             }</div>
